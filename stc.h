1a6b188dcac5fb2f9d8451668369ab5c725fbf1a
dinfuc510 <dauvaso18@gmail.com>
2024-10-26 11:29:49 +0700

    handle errors

--- stc.h
+++ stc.h
@@ -39,7 +39,9 @@ size_t strlen_with_bound(const char* str) {
 
 STC_DEF
 int stc_expand(Stc *stc, size_t len) {
-	assert(STC_MAX_LEN - stc->cap >= len && "ERROR: reached STC_MAX_LEN");
+	if (stc->cap + len >= STC_MAX_LEN) {
+		return 0; /* exceed limit */
+	}
 
 	if (stc->len + len >= stc->cap) {
 		if (stc->cap == 0) {
@@ -50,11 +52,14 @@ int stc_expand(Stc *stc, size_t len) {
 			stc->cap <<= 1;
 		}
 		if (stc->len + len >= stc->cap) {
-			return 0;
+			return 0; /* could not expand */
 		}
 
-		stc->buf = (char*) realloc(stc->buf, stc->cap);
-		assert(stc->buf	!= NULL && "ERROR: out of memory");
+		void *new_buf = realloc(stc->buf, stc->cap);
+		if (new_buf == NULL) {
+			return 0; /* out of memory */
+		}
+		stc->buf = (char*) new_buf;
 	}
 	return 1;
 }
@@ -84,42 +89,48 @@ void stc_pushf(Stc *stc, const char *fmt, ...) {
 	va_start(arg, fmt);
 
 	const char *start = fmt;
-	while (fmt - start < MAX_ITER && *fmt != '\0') {
+	while (fmt - start < STC_MAX_LEN && *fmt != '\0') {
 		size_t percent_idx = strcspn(fmt, "%");
 		stc_insert(stc, fmt, percent_idx, stc->len);
 		fmt += percent_idx;
-		if (*fmt == '\0' || fmt - start >= MAX_ITER) {
+		if (*fmt != '%' || fmt - start >= STC_MAX_LEN) {
 			break;
 		}
-		if (*fmt == '%') {
-			++fmt;
-			switch (*fmt) {
-				case 't': {
-					Stc *s = va_arg(arg, Stc*);
-					stc_insert(stc, s->buf, s->len, stc->len);
-					break;
+
+		fmt++;
+		switch (*fmt) {
+			case 't': {
+				Stc *s = va_arg(arg, Stc*);
+				stc_insert(stc, s->buf, s->len, stc->len);
+				break;
+			}
+			case 's': {
+				char *s = va_arg(arg, char*);
+				stc_insert(stc, s, strlen_with_bound(s), stc->len);
+				break;
+			}
+			case 'd': {
+				int n = va_arg(arg, int);
+				if (n <= 0) {
+					stc_insert(stc, "0", 1, stc->len);
 				}
-				case 's': {
-					char *s = va_arg(arg, char*);
-					stc_insert(stc, s, strlen_with_bound(s), stc->len);
-					break;
+				int len = 0;
+				while (n > 0) {
+					buff[len++] = n % 10 + '0';
+					n /= 10;
 				}
-				case 'd': {
-					int n = va_arg(arg, int);
-					int len = 0;
-					while (n > 0) {
-						buff[len++] = n % 10 + '0';
-						n /= 10;
-					}
-					while (len > 0) {
-						stc_insert(stc, &buff[--len], 1, stc->len);
-					}
-					break;
+				while (len > 0) {
+					stc_insert(stc, &buff[--len], 1, stc->len);
 				}
-				case '%': {
-					stc_insert(stc, "%", 1, stc->len);
-					break;
-				}
+				break;
+			}
+			case '%': {
+				stc_insert(stc, "%", 1, stc->len);
+				break;
+			}
+			default: {
+				assert(0 && "ERROR: unknow conversion");
+				break;
 			}
 		}
 		fmt++;
@@ -136,7 +147,7 @@ void stc_from_file(Stc *stc, const char *fp) {
 	}
 
 	char buff[STC_BUF_LEN];
-	while (fgets(buff, 1023, f) != NULL) {
+	while (fgets(buff, STC_BUF_LEN - 1, f) != NULL) {
 		stc_pushf(stc, "%s", buff);
 	}
 
